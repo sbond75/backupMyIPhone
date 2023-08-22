c411c7ce95eedb10b96f7f19f1abf2415dfaedd1
sbond75 <43617712+sbond75@users.noreply.github.com>
2023-08-22 17:29:36 -0400

    wip remote backups; about to test some stuff

--- shell_new_libimobiledevice.nix
+++ shell_new_libimobiledevice.nix
@@ -1,15 +1,14 @@
-{ pkgs ? (builtins.fetchTarball { # https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs :
+{ pkgs ? import (builtins.fetchTarball { # https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs :
   # Descriptive name to make the store path easier to identify
   name = "nixos-unstable-2020-09-03";
   # Commit hash for nixos-unstable as of the date above
   url = "https://github.com/NixOS/nixpkgs/archive/702d1834218e483ab630a3414a47f3a537f94182.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
   sha256 = "1vs08avqidij5mznb475k5qb74dkjvnsd745aix27qcw55rm0pwb";
-})}:
-#with pkgs;
-
-# Apply overlay (for raspberry pi to work)
-let myOverlay = (self: super: {
+}) {
+  # Apply overlay (for raspberry pi to work)
+  overlays = [
+    (self: super: {
       libxcrypt = if super.system == "armv7l-linux" then (super.libxcrypt.overrideAttrs (oldAttrs: rec {
         #doCheck = false;
         patchPhase = ''
@@ -18,17 +17,10 @@ let myOverlay = (self: super: {
           substituteInPlace test/alg-yescrypt.c "return retval;" "return 0;"
         '';
       })) else super.libxcrypt;
-
-      #libxcrypt = (self.pkgs.callPackage ./libxcrypt/libxcrypt.nix {version="4.4.36";});
-    });
-  nixpkgs = import pkgs {};
-  finalPkgs = import pkgs {
-    #system = if (nixpkgs.hostPlatform.isDarwin) then "x86_64-darwin" else builtins.currentSystem; # For M1 Mac to work
-    # Identity: overlays = [];
-    overlays = [ myOverlay ];
-  };
-in
-with finalPkgs;
+    })
+  ];
+}}:
+with pkgs;
 
 mkShell {
   buildInputs = [
