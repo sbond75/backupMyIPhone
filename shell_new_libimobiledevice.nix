698f2cfc0f083e15a15aa6d482e74ef80f72420b
sbond75 <43617712+sbond75@users.noreply.github.com>
2023-08-22 18:01:06 -0400

    wip remote backups; about to test some stuff

--- shell_new_libimobiledevice.nix
+++ shell_new_libimobiledevice.nix
@@ -5,53 +5,15 @@
   url = "https://github.com/NixOS/nixpkgs/archive/702d1834218e483ab630a3414a47f3a537f94182.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
   sha256 = "1vs08avqidij5mznb475k5qb74dkjvnsd745aix27qcw55rm0pwb";
-}) {
-  # Apply overlay (for raspberry pi to work)
-  overlays = [
-    (self: super: {
-      # libxcrypt = if super.system == "armv7l-linux" then (super.libxcrypt.overrideDerivation (oldAttrs: rec {
-    #     #doCheck = false;
-    #     # patchPhase = ''
-    #     #   echo "@@@@@@ in overlay"
-    #     #   #substituteInPlace Makefile.am "test/alg-yescrypt \\" ""
-    #     #   substituteInPlace test/alg-yescrypt.c "return retval;" "return 0;"
-    #     # '';
-
-    #     passsthru.enabledCryptSchemeIds = [
-    #   # https://github.com/besser82/libxcrypt/blob/v4.4.33/lib/hashes.conf
-    #   #"y"   # yescrypt
-    #   "gy"  # gost_yescrypt
-    #   "7"   # scrypt
-    #   "2b"  # bcrypt
-    #   "2y"  # bcrypt_y
-    #   "2a"  # bcrypt_a
-    #   "6"   # sha512crypt
-    # ];
-    #   })) else super.libxcrypt;
-
-      libxcrypt = if super.system == "armv7l-linux" then (super.libxcrypt.override (oldArgs: rec {
-        passsthru.enabledCryptSchemeIds = [
-      # https://github.com/besser82/libxcrypt/blob/v4.4.33/lib/hashes.conf
-      #"y"   # yescrypt
-      "gy"  # gost_yescrypt
-      "7"   # scrypt
-      "2b"  # bcrypt
-      "2y"  # bcrypt_y
-      "2a"  # bcrypt_a
-      "6"   # sha512crypt
-    ];
-      })) else super.libxcrypt;
-    })
-  ];
-}}:
+}) { }}:
 with pkgs;
 
 mkShell {
   buildInputs = [
-    (callPackage ./libimobiledevice/libimobiledevice_new.nix {
-      enablePython=false;
-      #enablePython=true; # doesn't work
-    })
+    # (callPackage ./libimobiledevice/libimobiledevice_new.nix {
+    #   enablePython=false;
+    #   #enablePython=true; # doesn't work
+    # })
     usbmuxd
     pkg-config
 
