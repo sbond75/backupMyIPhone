a7d9852421e30a06ba2817b7183cf72cf8be3b21
sbond75 <43617712+sbond75@users.noreply.github.com>
2023-08-22 16:39:32 -0400

    wip remote backups; about to test some stuff

--- shell_new_libimobiledevice.nix
+++ shell_new_libimobiledevice.nix
@@ -5,7 +5,7 @@
   url = "https://github.com/NixOS/nixpkgs/archive/702d1834218e483ab630a3414a47f3a537f94182.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
   sha256 = "1vs08avqidij5mznb475k5qb74dkjvnsd745aix27qcw55rm0pwb";
-})}:
+}) {} }:
 #with pkgs;
 
 # Apply overlay (for raspberry pi to work)
