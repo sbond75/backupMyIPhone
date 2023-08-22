2237b3275d700e7938b4c4ff7e4f4955734acda7
sbond75 <43617712+sbond75@users.noreply.github.com>
2023-08-22 18:31:17 -0400

    wip remote backups; about to test some stuff

--- shell_new_libimobiledevice.nix
+++ shell_new_libimobiledevice.nix
@@ -1,11 +1,20 @@
-{ pkgs ? import (builtins.fetchTarball { # https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs :
+{ pkgs ? (if system == "armv7l-linux" then ( import
+builtins.fetchTarball { # https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs :
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-unstable-2023-07-31";
+  # Commit hash for nixos-unstable as of the date above
+  url = "https://github.com/NixOS/nixpkgs/archive/ed92e2eb043d75e0417199d9650d5849527dd404.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "1iqc7rvrwbjg3wps9zzngvb9r47akzx8c39644xbhi1sxqf5rwrk";
+}
+) else (import builtins.fetchTarball { # https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs :
   # Descriptive name to make the store path easier to identify
   name = "nixos-unstable-2020-09-03";
   # Commit hash for nixos-unstable as of the date above
   url = "https://github.com/NixOS/nixpkgs/archive/702d1834218e483ab630a3414a47f3a537f94182.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
   sha256 = "1vs08avqidij5mznb475k5qb74dkjvnsd745aix27qcw55rm0pwb";
-}) { }}:
+})) { }}:
 with pkgs;
 
 let
