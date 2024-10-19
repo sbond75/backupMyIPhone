53d2358488aa1c615843fb678978c6c36b6cf5e6
sbond75 <43617712+sbond75@users.noreply.github.com>
2024-10-19 13:15:40 -0400

    wip

--- install_libimobiledevice_deps.sh
+++ install_libimobiledevice_deps.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install vim tmux git build-essential libxml2-dev python2.7 python2.7-dev fuse libtool autoconf libusb-1.0-0-dev libfuse-dev
+#sudo apt-get -y install vim tmux git build-essential libxml2-dev python2.7 python2.7-dev fuse libtool autoconf libusb-1.0-0-dev libfuse-dev
+sudo apt-get -y install vim tmux git build-essential libxml2-dev python3 python3-dev fuse libtool autoconf libusb-1.0-0-dev libfuse-dev
 
 # https://github.com/libimobiledevice/libimobiledevice
 sudo apt-get -y install \
