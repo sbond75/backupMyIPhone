a8ff88063bbd532284d15133887f5a88be15c028
sbond75 <43617712+sbond75@users.noreply.github.com>
2025-03-22 12:08:36 -0400

    wip

--- ibackupServer.py
+++ ibackupServer.py
@@ -117,7 +117,7 @@ def runCmd(argList):
     #print(result.stdout, '|', result.stderr)
     #return result
 
-    return subprocess.run(argList, shell=False, check=True)
+    return subprocess.run(argList, stdout=sys.stdout, stderr=sys.stderr, shell=False, check=True)
 
 def unmount(username_ftp):
     runCmd([sudoPath
