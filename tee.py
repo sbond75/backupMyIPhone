15a088421ae8a8bbf94554c05a11fbf582d7592b
sbond75 <43617712+sbond75@users.noreply.github.com>
2025-06-02 17:18:57 -0400

    wip python port for ibackupClient.sh to fix issues and simplify it to be just borg backup

--- tee.py
+++ tee.py
@@ -21,20 +21,20 @@ def setup_logging(logfile: str):
             env=env
         )
 
-        # Signal forwarding handler
-        def forward_signal(signum, frame):
-            # Forward the signal to child
-            try:
-                process.send_signal(signum)
-            except ProcessLookupError:
-                pass  # Process already exited
+        # # Signal forwarding handler
+        # def forward_signal(signum, frame):
+        #     # Forward the signal to child
+        #     try:
+        #         process.send_signal(signum)
+        #     except ProcessLookupError:
+        #         pass  # Process already exited
 
-        # Register signals to forward
-        signals_to_forward = [signal.SIGINT, signal.SIGTERM]
-        if hasattr(signal, "SIGHUP"):
-            signals_to_forward.append(signal.SIGHUP)
-        for sig in signals_to_forward:
-            signal.signal(sig, forward_signal)
+        # # Register signals to forward
+        # signals_to_forward = [signal.SIGINT, signal.SIGTERM]
+        # if hasattr(signal, "SIGHUP"):
+        #     signals_to_forward.append(signal.SIGHUP)
+        # for sig in signals_to_forward:
+        #     signal.signal(sig, forward_signal)
 
         # Open log file for writing
         assert process.stdout is not None
