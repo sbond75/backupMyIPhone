f6e8fd93dd11d131111962b974a0523d21e58f69
sbond75 <43617712+sbond75@users.noreply.github.com>
2025-06-02 17:27:08 -0400

    wip python port for ibackupClient.sh to fix issues and simplify it to be just borg backup

--- tee.py
+++ tee.py
@@ -42,8 +42,8 @@ def setup_logging(logfile: str):
             for line in process.stdout:
                 from datetime import datetime
                 timestamped = f"{datetime.now().strftime('%Y-%m-%d-%H:%M:%S.%f')} {line}"
-                print(timestamped, end='')       # Print to terminal
-                log_file.write(timestamped)      # Write to file
+                print(timestamped, end='', flush=True)       # Print to terminal
+                log_file.write(timestamped)                  # Write to file
 
         process.wait()
         sys.exit(process.returncode)
